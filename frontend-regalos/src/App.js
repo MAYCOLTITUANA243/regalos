1c1cd06faf6e1116508e1815bf20363062aa9e64
MAYCOLTITUANA243 <mtituana243@gmail.com>
2023-04-13 15:25:34 -0500

    .

--- frontend-regalos/src/App.js
+++ frontend-regalos/src/App.js
@@ -3,19 +3,20 @@ import { Routes, Route} from 'react-router-dom';
 import Layout from './pages/Layout';
 import Catalogo from './pages/productoSalida';
 import Home from './pages/home';
-import Logo from './img/logo.jpeg';
+import Logo from './img/logo.gif';
 function App(){
   return(
     <div className="App" class="bg-light">
       <div style={{
       display: 'flex',
-      justifyContent: 'center'
+      justifyContent: 'center',
+      background:'#22252C'
       }}>
         <img src={Logo} alt="Descripción de la imagen" 
         style={{
           alignContent:'center',
-          width: '100%',
-          height: '250px',
+          width: '150%',
+          height: '300px',
         }}/>
       </div>
       <Routes>
